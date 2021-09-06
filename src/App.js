ff2cada7dc730d6fa9c7355d76a31c8a9fed982d
fumiya <altema1217bbfumi@live.jp>
2021-09-06 13:42:52 +0900

    update: app.js react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (
     <BrowserRouter>
       <h1>Hello React Router</h1>
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/contact">
-        <Contact />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
     </BrowserRouter>
   )
 }
@@ -27,6 +32,10 @@ const About = () => {
 
 const Contact = () => {
   return <h2>Contact</h2>
+}
+
+const NotFound = () => {
+  return <h2>Not found</h2>
 }
 
 export default App
